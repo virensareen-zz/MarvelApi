6c2160785c257e4dbe4ad1219a8d4b3ff96e98fd
virensareen <viren.sareen@gmail.com>
2020-04-27 23:11:14 +0100

    Cleaned up comments

--- marvel/src/main/java/service/MarvelApiService.java
+++ marvel/src/main/java/service/MarvelApiService.java
@@ -69,11 +69,7 @@ public class MarvelApiService {
     private static String getMd5(String input)
     {
         try {
-            // Static getInstance method is called with hashing MD5
             MessageDigest md = MessageDigest.getInstance("MD5");
-
-            // digest() method is called to calculate message digest
-            //  of an input digest() return array of byte
             byte[] messageDigest = DigestUtils.md5Digest(input.getBytes("UTF-8"));
 
             // Convert to string
